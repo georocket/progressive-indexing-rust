7bdc4cb7bfc3a19069575c24947cf45c24c79b3b
derpadi <patrick.goehringer@stud.tu-darmstadt.de>
2023-07-16 15:35:19 +0200

    Encapsulating query-part into QueryEngine

---
+++ src/QueryEngine.rs
@@ -0,0 +1,122 @@
+use std::{fs::File, path::Path, io::{BufWriter, BufRead, BufReader}};
+
+use grep::{regex::RegexMatcher, searcher::{Sink, Searcher, SinkMatch, SinkFinish}};
+use std::io::Write;
+
+
+
+
+struct QueryEngine {
+    filename: String,
+    file: File,
+    file_format: FileFormat,
+    offset_list: Vec<(u64, u64)>,
+    num_rows: usize
+}
+
+impl QueryEngine {
+    fn new(&mut self, filename: String) -> QueryEngine {
+        let file = File::open(&filename).expect("Error opening file!");
+        let file_format = FileFormat::CityGML; // At the moment hard coded
+        let mut offset_list:Vec<(u64, u64)> = Vec::new();
+        let num_rows = 0;
+
+        // Initialize Offset List
+        let offset_list_filename = filename.clone() + ".qry";
+        if !Path::new(&offset_list_filename).exists() {
+            // TODO: Create offset-list and store it to file
+            let matcher = RegexMatcher::new_line_matcher("cityObjectMember>");
+            let sink = OffsetSink{output_filename: offset_list_filename, counter: 0, first: 0, data: &mut offset_list};
+            
+        } else {
+            // TODO: Load Offset-List from file
+        }
+
+        QueryEngine {
+            filename,
+            file: file, 
+            file_format: file_format, 
+            offset_list: offset_list,
+            num_rows: num_rows
+        }
+
+    }
+
+    fn read_offset_list(&self, offset_list: &mut Vec<(u64, u64)>) {
+        let offset_list_filename = self.filename.clone() + ".qry";
+        let file = File::open(offset_list_filename).expect("Error opening offset-list file!");
+        let r = BufReader::new(file);
+
+        for l in r.lines() {
+            let line = l.expect("Error reading line!");
+            let values: Vec<u64> = line
+                                        .split(',')
+                                        .map(
+                                            |x| x.trim().parse::<u64>().expect("Error parsing value")
+                                        ).collect();
+            if values.len() != 2 {
+                println!("Incorrect offset-list file!");
+            }
+            let first = values[0];
+            let second = values[1];
+            
+            offset_list.push((first, second));
+        }
+    }
+}
+
+struct OffsetSink<'a>{
+    output_filename: String,
+    counter: u64,
+    first: u64,
+    data: &'a mut Vec<(u64,u64)>
+}
+
+impl OffsetSink<'_>{
+    fn print_num_results(&self){
+        println!("{}", self.counter/2);
+    }
+
+    //fn write_data_to_file(&self, file_name: &str) -> &Vec<(u64,u64)>{
+    //    let f = File::create("offset.bin").expect("Error creating file!");
+    //    let mut w = BufWriter::new(f);
+    //    let mut buffer:Vec<u8> = vec![];
+    //    for d in self.data{
+    //        buffer.extend_from_slice(&d.0.to_be_bytes());
+    //        buffer.extend_from_slice(&d.1.to_be_bytes());
+    //    }
+    //    w.write_all(&buffer).expect("Error writing!");
+    //    return &self.data;
+    //}
+}
+
+impl Sink for OffsetSink<'_> {
+    type Error = std::io::Error;
+
+    fn matched(&mut self, _: &Searcher, line: &SinkMatch) -> Result<bool, Self::Error> {
+        // Case: 
+        if self.counter % 2 == 0 {
+            self.first = line.absolute_byte_offset();
+        } else {
+            self.data.push((self.first,line.absolute_byte_offset()));
+        }
+        self.counter += 1;
+        Ok(true)
+    }
+
+    fn finish(&mut self, _searcher: &Searcher, _: &SinkFinish) -> Result<(), Self::Error> {
+        let mut f = File::create(&mut self.output_filename).expect("Error creating file!");
+        
+        for i in (1..self.data.len()-1).step_by(2){
+            write!(f, "{},{}\n", self.data[i].0, self.data[i].1).expect("Error writing offsetlist!");
+        }
+        println!("Wrote data to file!");
+        self.print_num_results();
+        Ok(())
+    }
+
+}
+
+enum FileFormat {
+    CityGML, CityJSON
+}
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,72 @@
 mod file_buffer;
+mod QueryEngine;
 use std::fs::{File, self};
-use std::io::{Read, Write, Seek};
+use std::io::{Read, Write, BufWriter, BufRead, BufReader};
 use std::os::unix::prelude::FileExt;
 use std::time::Instant;
 use std::vec;
-use grep::printer::{StandardSink, StandardBuilder};
+use grep::matcher::Matcher;
+use grep::printer::{StandardBuilder, ColorSpecs, StandardSink, SummarySink};
 use grep::regex::RegexMatcher;
-use grep::searcher::{SearcherBuilder, SinkMatch, SinkFinish};
+use grep::searcher::{SearcherBuilder, SinkMatch, SinkFinish, MmapChoice, BinaryDetection};
 use grep::searcher::Sink;
+use grep::searcher::sinks::UTF8;
 use grep::searcher::Searcher;
-use std::{mem, slice};
+use grep::cli;
+use termcolor::ColorChoice;
 
 fn main() {
     const TESTFILE_1: &str = "/home/derpadi/Documents/Work/Fraunhofer_IGD/ProgressiveIndexingRust/src/DA12_3D_Buildings_Merged.gml";
+
+    //tmp();
+
+}
+
+fn search(pattern: &str, filename: &str) {
+    let file = File::open(filename).expect("Error opening file!");
+    let matcher = RegexMatcher::new_line_matcher(&pattern).expect("Error");
+    let mut searcher = SearcherBuilder::new()
+        .binary_detection(BinaryDetection::quit(b'\x00'))
+        .line_number(false)
+        .build();
+
+    //let mut printer = StandardBuilder::new()
+    //    .color_specs(ColorSpecs::default_with_color())
+    //    .build(cli::stdout(if cli::is_tty_stdout() {
+    //        ColorChoice::Auto
+    //    } else {
+    //        ColorChoice::Never
+    //    }));
+
+    
+    let mut printer = StandardBuilder::new()
+        .color_specs(ColorSpecs::default_with_color())
+        .build(cli::stdout(if cli::is_tty_stdout() {
+            ColorChoice::Auto
+        } else {
+            ColorChoice::Never
+        }));
+
+        
+    let start = Instant::now();
+    let res = searcher.search_file(&matcher, &file, printer.sink(&matcher));
+    let end = start.elapsed();
+    println!("Time elapsed: {:?}", end);
+}
+
+fn tmp(){
+    const TESTFILE_1: &str = "/home/derpadi/Documents/Work/Fraunhofer_IGD/ProgressiveIndexingRust/src/DA12_3D_Buildings_Merged.gml";
     let mut f = file_buffer::FileBuffer::new(TESTFILE_1, 1024*1024).expect("Error!");
-    //for i in 0..5{
-    //    let s = Instant::now();
-    //    create_offset_list(TESTFILE_1, "offsetList.txt");
-    //    let d = s.elapsed();
-    //    println!("Runtime: {:?}", d);
-    //}
-    let mut load_data = vec![0u64];
+    for i in 0..5{
+        let s = Instant::now();
+        create_offset_list(TESTFILE_1, "offsetList.txt");
+        let d = s.elapsed();
+        println!("Runtime: {:?}", d);
+    }
+    let mut load_data = vec![];
     read_into_vec64("offset.bin", &mut load_data);
-    println!("{:?}", load_data);
+
+    //println!("{:?}", load_data);
 }
 
 
@@ -30,11 +74,19 @@ fn create_offset_list(filename: &str, output: &str) {
     let pattern = "cityObjectMember>";
     let matcher = RegexMatcher::new_line_matcher(pattern).unwrap();
     let file = File::open(filename).expect("Error opening file!");
-    let mut sink = CustomSink{output_filename: "OffsetList.txt".to_string(), counter: 0, data: vec![0u64, 0]};
+    //let bufReader = BufReader::new(file);
+    
+    let mut sink = CustomSink{output_filename: "OffsetList.txt".to_string(), counter: 0, data: vec![]};
 
-    let mut searcher = SearcherBuilder::new().line_number(true).build();
+    
+    let mut searcher = SearcherBuilder::new()
+    .line_number(true)
+    .multi_line(true)
+    .build();
 
+    let start = Instant::now();
     searcher.search_file(&matcher, &file, &mut sink).expect("Something went wrong!");
+    println!("Time for wrtiing: {:?}", start.elapsed());
     sink.write_data_to_file("offset.bin");
 }
 
@@ -55,15 +107,18 @@ fn read_into_vec64(filename: &str, vec: &mut Vec<u64>) {
 }
 
 impl CustomSink{
-    fn print_num_results(&mut self){
+    fn print_num_results(&self){
         println!("{}", self.counter/2);
     }
 
-    fn write_data_to_file(&mut self, file_name: &str) -> &Vec<u64>{
-        let mut f = File::create("offset.bin").expect("Error creating file!");
+    fn write_data_to_file(&self, file_name: &str) -> &Vec<u64>{
+        let f = File::create("offset.bin").expect("Error creating file!");
+        let mut w = BufWriter::new(f);
+        let mut buffer:Vec<u8> = vec![];
         for &d in &self.data{
-            f.write_all(&d.to_be_bytes());
+            buffer.extend_from_slice(&d.to_be_bytes());
         }
+        w.write_all(&buffer).expect("Error writing!");
         return &self.data;
     }
 }
